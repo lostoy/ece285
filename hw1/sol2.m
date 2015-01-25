03a3d91420cf4e2baadd15b87e175be4a4606c7e
lostoy <lostoy.li@gmail.com>
2015-01-25 00:33:10 -0800

    correct error distribution for A

---
+++ hw1/sol2.m
@@ -0,0 +1,14 @@
+%% plot histograms
+
+dist_name=cell(1,4);
+dist_name{1}='uniform';
+dist_name{2}='gaussian';
+dist_name{3}='1_-1';
+dist_name{4}='cauchy';
+maxIter=1000;
+for dist=1:4
+    disp(['----' dist_name{dist}]);
+    for iter=1:maxIter
+        
+    end
+end
