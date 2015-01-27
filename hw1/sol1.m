bdeba32244155f592f73783cbb0fc8427b06d9d8
lostoy <lostoy.li@gmail.com>
2015-01-26 21:02:40 -0800

    serious error corrected

--- hw1/sol1.m
+++ hw1/sol1.m
@@ -8,20 +8,17 @@ maxIter=1000;
 maxSupp=10;
 dist_name=cell(1,3);
 
-dist_name{1}='gaussian';
-dist_name{2}='1_-1';
-dist_name{3}='cauchy';
-for dist=1:3
+dist_name{1}='uniform';
+dist_name{2}='gaussian';
+dist_name{3}='1_-1';
+dist_name{4}='cauchy';
+
+options.min_error=1e-4;
+options.t=0.5;
+runTimes=zeros(1,5);
+for dist=1:4
     disp(['----' dist_name{dist}]);
-    switch dist
-        case 1
-            A=randn(n,m);
-        case 2
-            A=(rand(n,m)>0.5)*2-1;
-        case 3
-            A=trnd(1,n,m);
-    end        
-    A=normc(A);
+    
     
     err1_MP=zeros(maxSupp,maxIter);
     err2_MP=zeros(maxSupp,maxIter);
@@ -41,45 +38,58 @@ for dist=1:3
     for supp_num=1:maxSupp
         disp(['-------- supp= ' num2str(supp_num) ' / ' num2str(maxSupp)])
         for iter=1:maxIter
+            A=randn(n,m);
+            A=normc(A);
+            
             supp_ind=randperm(m,supp_num);
             supp=zeros(m,1);
             supp(supp_ind)=1;
             x0=zeros(m,1);
-            tp=rand>0.5;
+            switch dist
+                case 1
+                    tp=rand>0.5;
+                    x0(supp_ind)=tp*(rand(supp_num,1)-2)+(1-tp)*(rand(supp_num,1)+2);
+                case 2
+                    x0(supp_ind)=randn(supp_num,1);
+                case 3
+                    x0(supp_ind)=(rand(supp_num,1)>0.5)*2-1;
+                case 4
+                    x0(supp_ind)=trnd(1,supp_num,1);
+
+            end
             
-            x0(supp_ind)=tp*(rand(supp_num,1)-2)+(1-tp)*(rand(supp_num,1)+2);
             b=A*x0;
             
             %% test various algo
-            options.min_error=1e-4;
+            tic;
             [ x, S, r ]=MP(A,b,options);
-            err1_MP(supp_num,iter)=norm(x-x0)/norm(x0);
+            err1_MP(supp_num,iter)=norm(x-x0)^2/norm(x0)^2;
             err2_MP(supp_num,iter)=(max(sum(S),supp_num)-sum(S.*supp))/max(sum(S),supp_num);
+            runTimes(1)=runTimes(1)+toc;
             %
-            options.min_error=1e-4;
+            tic
             [ x, S, r ]=OMP(A,b,options);
-            err1_OMP(supp_num,iter)=norm(x-x0)/norm(x0);
+            err1_OMP(supp_num,iter)=norm(x-x0)^2/norm(x0)^2;
             err2_OMP(supp_num,iter)=(max(sum(S),supp_num)-sum(S.*supp))/max(sum(S),supp_num);
-            
+            runTimes(2)=runTimes(2)+toc;
             %
-            options.min_error=1e-4;
+            tic
             [ x, S, r ]=LSOMP(A,b,options);
-            err1_LSOMP(supp_num,iter)=norm(x-x0)/norm(x0);
+            err1_LSOMP(supp_num,iter)=norm(x-x0)^2/norm(x0)^2;
             err2_LSOMP(supp_num,iter)=(max(sum(S),supp_num)-sum(S.*supp))/max(sum(S),supp_num);
-            
+            runTimes(3)=runTimes(3)+toc;
             %
-            options.min_error=1e-4;
-            options.t=0.5;
+            tic
             [ x, S, r ]=WMP(A,b,options);
-            err1_WMP(supp_num,iter)=norm(x-x0)/norm(x0);
+            err1_WMP(supp_num,iter)=norm(x-x0)^2/norm(x0)^2;
             err2_WMP(supp_num,iter)=(max(sum(S),supp_num)-sum(S.*supp))/max(sum(S),supp_num);
-            
+            runTimes(4)=runTimes(4)+toc;
             %
-            options.min_error=1e-4;
+            tic
             [ x, S, r ]=ThMP(A,b,options);
-            err1_ThMP(supp_num,iter)=norm(x-x0)/norm(x0);
+            err1_ThMP(supp_num,iter)=norm(x-x0)^2/norm(x0)^2;
             err2_ThMP(supp_num,iter)=(max(sum(S),supp_num)-sum(S.*supp))/max(sum(S),supp_num);
-            
+            runTimes(5)=runTimes(5)+toc;
         end
     end
     %% plot
@@ -100,7 +110,8 @@ for dist=1:3
     
     axis_supp_num=1:maxSupp;
     figure;
-    plot(axis_supp_num,err1_MP_mean,axis_supp_num,err1_OMP_mean,axis_supp_num,err1_LSOMP_mean,axis_supp_num,err1_WMP_mean,axis_supp_num,err1_ThMP_mean,'LineWidth',2);
+    %plot(axis_supp_num,err1_MP_mean,axis_supp_num,err1_OMP_mean,axis_supp_num,err1_LSOMP_mean,axis_supp_num,err1_WMP_mean,axis_supp_num,err1_ThMP_mean,'LineWidth',2);
+    plot(axis_supp_num,err1_MP_mean,axis_supp_num,err1_OMP_mean,axis_supp_num,err1_LSOMP_mean,axis_supp_num,err1_WMP_mean,'LineWidth',2);
     title('l2 recovery error v.s. Cardinality','FontSize',15,'FontWeight','Bold')
     xlabel('Cardinality of the true solution','FontSize',15);
     ylabel('Average relative l_2 error','FontSize',15);
@@ -118,3 +129,14 @@ for dist=1:3
     saveas(gca, ['./eps/' char(dist_name(dist)) '_card.eps'] ,'epsc');
     
 end
+%%
+figure;
+runTimes=runTimes/maxIter/4;
+bar(runTimes);
+
+title('runtimes of the algorithms','FontSize',15,'FontWeight','Bold')
+xlabel('Algorithms to compare','FontSize',15);
+ylabel('runtime(s)','FontSize',15);
+set(gca,'XTickLabel',{'MP','OMP','LS-OMP','Weak-MP(t=0.5)','Thresholding'})
+set(gca,'FontSize',10,'FontWeight','Bold');
+saveas(gca, ['./eps/runtime.eps' ] ,'epsc');
